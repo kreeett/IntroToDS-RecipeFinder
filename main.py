f53df37c924e71428f7689a2bca73f0ea5fea5a5
kreeett <sshougen@gmail.com>
2025-04-29 20:34:06 +0300

    get ingred function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 recipes = {}
-def fillRec(name: str,cuisine: str,ingredients: list,perpTime: int,diff:int,rating:int):
+def fillRec(name: str,cuisine: str,ingredients: list,perpTime: int,diff:int,rating:float):
     if recipes.get(name,0) is not 0:
         return -1
     recipes[name]['cuisine'] = cuisine
@@ -7,5 +7,14 @@ def fillRec(name: str,cuisine: str,ingredients: list,perpTime: int,diff:int,rati
     recipes[name]['difficulty']=diff
     recipes[name][perpTime] = perpTime
     recipes[name][rating]= rating
+def getIngred(name):
+    out =[]
+    while 1:
+        x = input("Enter ingredients(leave empty to exit): ")
+        if(x==''):
+            break
+        out.append(x)
+    return out
+
     
         
