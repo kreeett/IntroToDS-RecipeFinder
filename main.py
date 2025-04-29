571d1b24eafa9136af43ef908034f3db8241e23d
kreeett <sshougen@gmail.com>
2025-04-29 20:26:05 +0300

    Fill function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 recipes = {}
+def fillRec(name: str,cuisine: str,ingredients: list,perpTime: int,diff:int,rating:int):
+    if recipes.get(name,0) is not 0:
+        return -1
+    recipes[name]['cuisine'] = cuisine
+    recipes[name]['ingredients']=ingredients
+    recipes[name]['difficulty']=diff
+    recipes[name][perpTime] = perpTime
+    recipes[name][rating]= rating
+    
+        
